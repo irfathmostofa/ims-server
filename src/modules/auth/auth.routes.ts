e189b5d93e01a119952c83e8a2fd0823a0c6880a
irfathmostofa <irfathmostofa1@gmail.com>
2025-09-22 13:05:38 +0600

    new update

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -3,5 +3,5 @@ import { login, profile } from "./auth.controller";
 
 export default async function authRoutes(fastify: FastifyInstance) {
   fastify.post("/login", login);
-  fastify.post("/profile", { preHandler: [fastify.authenticate] }, profile);
+  fastify.get("/profile", { preHandler: [fastify.authenticate] }, profile);
 }
