093af5045f7873ce6d28a9b894ca2b56eebfa040
irfathmostofa <irfathmostofa1@gmail.com>
2025-09-23 09:35:23 +0600

    new

--- src/modules/setup/setup.model.ts
+++ src/modules/setup/setup.model.ts
@@ -2,23 +2,14 @@ import { CrudModel } from "../../core/models/crud.model";
 
 export const companyModel = new CrudModel(
   "company",
-  [
-    "code",
-    "name",
-    "address",
-    "phone",
-    "email",
-    "logo",
-    "website",
-    "created_by",
-  ],
+  ["code", "name", "address", "phone", "email", "logo", "website"],
   ["code", "name", "address", "phone", "email"]
 );
 
 export const brancheModel = new CrudModel(
   "branch",
   ["code", "company_id", "name", "type", "address", "phone", "created_by"],
-  ["code", "company_id", "name", "type", "address", "phone", "created_by"]
+  ["code", "company_id", "name"]
 );
 export const roleModel = new CrudModel(
   "role",
