2440b1caa8208b4f8e6cf74a5ce1b64734fda3f3
MP-ANNY <mahmodaanny@gmail.com>
2025-10-22 17:20:19 +0600

    mahmoda the boss

--- src/modules/party/party.model.ts
+++ src/modules/party/party.model.ts
@@ -1,7 +1,7 @@
 import { CrudModel } from "../../core/models/crud.model";
 export const partyModel = new CrudModel(
   "party",
-  ["code", "branch_id", "type", "name", "phone"],
+  ["code", "branch_id", "type", "phone"],
   ["name", "phone", "email"],
-  ["address", "email", "credit_limit", "loyalty_points", "status"]
+  ["name","address", "email", "credit_limit", "loyalty_points", "status"]
 );
