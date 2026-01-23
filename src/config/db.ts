1ff0a9692212f4c7cc289c1e380b58c35e0fba22
irfathmostofa <irfathmostofa1@gmail.com>
2026-01-23 15:19:17 +0600

    new update

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,8 +1,33 @@
-import { Pool } from "pg";
-import { ENV } from "./env";
+// import { Pool } from "pg";
+// import { ENV } from "./env";
+
+// const pool = new Pool({
+//   connectionString: ENV.DATABASE_URL,
+// });
+
+// export default pool;
+
+import { Pool } from 'pg'
+import { ENV } from './env'
 
 const pool = new Pool({
-  connectionString: ENV.DATABASE_URL,
-});
+  host: ENV.DB_HOST,
+  port: ENV.DB_PORT,
+  database: ENV.DB_NAME,
+  user: ENV.DB_USER,
+  password: ENV.DB_PASSWORD,
 
-export default pool;
+  max: 5,            // low RAM safe
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+})
+
+// optional: verify once
+pool.query('SELECT 1')
+  .then(() => console.log('✅ PostgreSQL connected'))
+  .catch(err => {
+    console.error('❌ PostgreSQL connection error', err)
+    process.exit(1)
+  })
+
+export default pool
--- src/config/env.ts
+++ src/config/env.ts
@@ -6,4 +6,9 @@ export const ENV = {
   PORT: process.env.PORT || 5000,
   DATABASE_URL: process.env.DATABASE_URL as string,
   JWT_SECRET: process.env.JWT_SECRET as string,
+  DB_HOST: process.env.DB_HOST!,
+  DB_PORT: Number(process.env.DB_PORT || 5432),
+  DB_NAME: process.env.DB_NAME!,
+  DB_USER: process.env.DB_USER!,
+  DB_PASSWORD: process.env.DB_PASSWORD!,
 };
