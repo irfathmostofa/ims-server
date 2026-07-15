3c24504d18052f5e12905fbdea2e35f3d6e9bc58
irfathmostofa <irfathmostofa1@gmail.com>
2026-07-15 11:35:44 +0600

    new update

--- src/modules/party/party.model.ts
+++ src/modules/party/party.model.ts
@@ -2,6 +2,6 @@ import { CrudModel } from "../../core/models/crud.model";
 export const partyModel = new CrudModel(
   "party",
   ["code", "branch_id", "type", "phone"],
-  ["name", "phone", "email"],
-  ["name","address", "email", "credit_limit", "loyalty_points", "status"]
+  ["phone", "email"],
+  ["name", "address", "email", "credit_limit", "loyalty_points", "status"],
 );
